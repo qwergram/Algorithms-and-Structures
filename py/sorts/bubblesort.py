16bcbf312a2dbba3fb26c7c1f58c96313a4031b2
Norton Pengra <npengra317@gmail.com>
2016-10-25 19:51:49 -0700

    syntax error

--- py/sorts/bubblesort.py
+++ py/sorts/bubblesort.py
@@ -1,5 +1,5 @@
 def bubblesort(l):
-    for i in range(0, len(l))-1):
+    for i in range(0, len(l)-1):
         for i in range(1, len(l)):
             if l[i-1] > l[i]:
                 l[i-1], l[i] = l[i], l[i-1]
