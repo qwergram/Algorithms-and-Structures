ba59b46b3ba9ded1d6c6dae4bb48794dbe7d7451
Norton Pengra <npengra317@gmail.com>
2016-10-25 23:26:41 -0700

    expand node properties

--- py/structures/doublylinkedlist.py
+++ py/structures/doublylinkedlist.py
@@ -2,12 +2,47 @@ class Node:
     
     def __init__(self, value, prev=None, next=None):
         self.value = value
-        self.next = next
-        self.prev = prev
+        self._next = None
+        self._prev = None
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next_setter(self, value):
+        self._next = value
+        value._prev = self
+
+    @property
+    def prev(self):
+        return self._prev
+
+    @prev.setter
+    def prev_setter(self, value):
+        self._prev = value
+        value._next = self
+
+
 
 class DoublyLinkedList:
 
     def __init__(self, value=None):
+        self.size = 0
         if value is None:
             self.head = self.tail = None
+        else:
+            self.head = self.tail = Node(value)
+            self.size += 1
+
+    def add(self, value):
+        self.size += 1
+        if self.head:
+            cursor = self.head
+            while cursor.next:
+                cursor = cursor.next
+            cursor.next = Node(value)
+        else:
+            self.head = Node(value)
         
+    
