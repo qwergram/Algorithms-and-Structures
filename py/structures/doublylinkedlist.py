0bc55203c0d4cafda530916e9e16c38004107ffb
Norton Pengra <npengra317@gmail.com>
2016-10-26 08:35:41 -0700

    create add, peek and remove

--- py/structures/doublylinkedlist.py
+++ py/structures/doublylinkedlist.py
@@ -37,12 +37,54 @@ class DoublyLinkedList:
 
     def add(self, value):
         self.size += 1
-        if self.head:
-            cursor = self.head
-            while cursor.next:
-                cursor = cursor.next
-            cursor.next = Node(value)
+        if self.tail and self.head:
+            new_node = Node(value)
+            self.tail.next = new_node
+            self.tail = new_node
         else:
-            self.head = Node(value)
+            self.head = self.tail = Node(value)
         
+    def peek(self, index):
+        if index < (self.size // 2):
+            cursor = self.head
+            for i in range(index):
+                cursor = cursor.next
+            return cursor.value
+        elif index < self.size:
+            cursor = self.tail
+            for i in range(self.size - index):
+                cursor = cursor.prev
+            return cursor.value
+        raise IndexError('list index out of range')
+
+    def remove(self, index, r=False):
+        if index < (self.size // 2):
+            cursor = self.head
+            if index != 0:
+                for i in range(index - 1):
+                    cursor = cursor.next
+                to_delete = cursor.next
+                cursor.next = cursor.next.next
+            else:
+                to_delete = self.head
+                self.head = self.head.next
+        elif index < self.size:
+            reverse_index = self.size - index - 1
+            cursor = self.tail
+            if reverse_index != 0:
+                for i in range(reverse_index):
+                    cursor = cursor.prev
+                to_delete = cursor.prev
+                cursor.prev = cursor.prev.prev
+            else:
+                to_delete = self.tail
+                self.tail = self.tail.prev
+        else:
+            raise IndexError('list index out of range')    
+        self.size -= 1
+        return to_delete.value if r else None
+        
+
+            
+            
     
