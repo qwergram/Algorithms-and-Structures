763ad9e43a2a3593cb21ad2a0c3c76930f5a4fa0
Norton Pengra <npengra317@gmail.com>
2016-10-31 12:14:05 -0700

    create inplaceQuicksort

--- py/sorts/quicksort.py
+++ py/sorts/quicksort.py
@@ -1,7 +1,7 @@
 # A quicksort
 # https://en.wikipedia.org/wiki/Quicksort
 
-
+# According to MS, this is wrong
 def recursiveQuicksort(values):
     if values:
         pivot, smaller, larger = values[0], [], []
@@ -12,3 +12,20 @@ def recursiveQuicksort(values):
                 larger.append(item)
         return recursiveQuicksort(smaller) + [pivot] + recursiveQuicksort(larger)
     return values
+
+
+
+def inplaceQuicksort(values, begin=0, end=None):
+    def partition(array, begin, end):
+        pivot = begin
+        for i in range(begin+1, end+1):
+            if array[i] <= array[begin]:
+                pivot += 1
+                array[i], array[pivot] = array[pivot], array[i]
+        array[pivot], array[begin] = array[begin], array[pivot]
+        return pivot
+    end = len(values) - 1 if end is None else end
+    if begin >= end: return
+    pivot = partition(array, begin, end)
+    quicksort(array, begin, pivot-1)
+    quicksort(array, pivot+1, end)
